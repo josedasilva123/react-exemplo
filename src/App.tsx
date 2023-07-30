6ac336e43b181c60133fe8461978b60d9a45257b
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-07-29 22:31:41 -0300

    changing welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Olá galerinha nota 1000! Vamos aprender MUITO React juntos!</h1>
     </div>
   );
 }
